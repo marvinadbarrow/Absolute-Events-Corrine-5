9631e1dee6903c445f77e927acbe59f779e559ee
Marvin <bugsbunny@gmail.com>
2023-11-11 17:30:10 +0000

    fixed dropdown services submenu on mobile view, so that, when user moves mouse away from dropped submenu directly, or if the submenu is open and user hovers over a different link in the header navigation the submenu will close

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ let serviceList = document.getElementById('services-list-container')
 let serviceLink = document.getElementById('header-link')
 let mobileServiceLinks = document.querySelectorAll('.services-menu-mobile')
 let aboutEl = document.getElementById('about')
+let mobileServicesContainer = document.getElementById('mobile-services-container')
 console.log(mobileServiceLinks)
 console.log('nav bar')
 console.log(navbarEl)
 
 console.log('header nav')
-console.log(headerNavEl)
+let headerNavElements = headerNavEl.children[2].childNodes
+
 
 
 
@@ -21,7 +23,6 @@ console.log(window.innerWidth)
 
 // on scroll down 
 window.onscroll = () =>{
-console.log(window.innerWidth)
   // only execute animation if window is outside of media query limit
   // this only works if the window width is greater than 1000; but there's the issue that if user has already scrolled down, and THEN the window width goes below 1000, you won't be able to undo the effect that has already happened because both scroll up and scroll down can only happen when the window width is greater than 1000.  So if window width changes between scrolls, the second effect won't
   if(window.innerWidth >= 1000){
@@ -49,17 +50,21 @@ serviceLink.style.cssText = 'padding: 10px 10px;'
    headerNavEl.style.animation =  'headerNav2OpacityDown 1.4s ease-out backwards'
   lowerBtn.style.display = 'none'
 lowerBtn.style.animation = 'btnUnOpacity 1.4s ease-out forwards'
-serviceLink.style.cssText = 'padding: 10px 30px;'
+if(!serviceLink.style.display == 'none'){
+  serviceLink.style.cssText = 'padding: 10px 30px;'
+}
+
 headerNavEl.style.zIndex = 1;
 }
 
   }else{
 lowerBtn.style.display = 'none'
-console.log(headerNavEl)
 headerNavEl.style.animation = ''
 headerNavEl.style.zIndex = 1;
 navbarEl.style.cssText = 'display:block; display:flex; position:fixed; top:0;'
-serviceLink.style.cssText = 'padding: 10px 30px;'
+if(serviceLink){
+  serviceLink.style.cssText = 'padding: 10px 30px;'
+}
   }
 
 
@@ -87,26 +92,29 @@ const serviceHoverSmallScreen = (event) =>{
   console.log('times hovered')
   timesHoveredSmall +=1;
   console.log(timesHovered)
-  // hide the drop down
+  // hide the regular drop down
   serviceList.style.cssText = 'display:none;'
-let submenuOpen = 0
+
+  mobileServicesContainer.style.display = 'block'
       // loop through node list, and on each element, toggle display none/block
-      mobileServiceLinks.forEach(link =>{
-        if(link.style.display == 'block'){
-
-          link.style.display = 'none'
-
-          submenuOpen = 0
-        }else{
-          link.style.animation = 'btnOpacity 0.5s ease-out forwards'
-          link.style.display = 'block'
-
-        submenuOpen = 1
-        }
 
 
+      // mobileServiceLinks.forEach(link =>{
+      //   if(link.style.display == 'block'){
 
-      })
+      //     link.style.display = 'none'
+
+
+      //   }else{
+      //     link.style.animation = 'btnOpacity 0.5s ease-out forwards'
+      //     link.style.display = 'block'
+
+
+      //   }
+
+
+
+      // })
  
 
 
@@ -128,6 +136,7 @@ console.log(currentWidth)
 // show header nav in its normal state if screen moves over minimum size threshold. Without this, if it has been toggled to display:none when the screen is small, if the screen is resized to large, the header nav will remain hidden. 
 if(currentWidth > 999){
   headerNavEl.style.display = 'flex'
+  mobileServicesContainer.style.display = 'none'
 }
 
 })
@@ -163,25 +172,30 @@ serviceEl.addEventListener('mouseout', (event) =>{
   // for regular sized screens
   serviceList.style.cssText = 'display:none;'
   serviceList.classList.add('transform-menu')
-
-  // for screens that meet the media query consitions
-  mobileServiceLinks.forEach(link =>{
-      link.style.display = 'none'
-    })
-
 } )
 
+// on every header nav link, except the services link, on mouseover hide the container for services submenu on mobile display
 
-   
+headerNavElements.forEach(element => {
+  if(element.tagName == 'A' && element.id == ''){
+console.log(element)
+console.log(element.innerText)
+element.addEventListener('mouseover', () =>{
+  mobileServicesContainer.style.display = 'none'
+})
+  }
+});
 
+
+// when moving away from mobile services container hide the element
+
+mobileServicesContainer.addEventListener('mouseleave', () =>{
+  mobileServicesContainer.style.display = 'none'
+})
 hamburgerEl.addEventListener('click', (e) =>{
   e.preventDefault()
 
-  console.log('checking window innerWidth')
-  console.log(window.innerWidth)
-
-
-    console.log('menu click')
+  // toggle nav menu on and off
     if(headerNavEl.style.display == 'flex'){
       if(window.innerWidth < 1000){
       headerNavEl.style.display = 'none'
@@ -201,3 +215,7 @@ hamburgerEl.addEventListener('click', (e) =>{
 // add display block on hover for service menu mobile display since, moving away from the parent element causes the submenu options to close
 
 // mobileServiceLinks.forEach{link}
+
+// MOBILE SERVICES SUB MENU HIDE - just realized what to do....  I guess you want the submenu to be open while you are hovering over the 'services' heading so, but when you mouseout, have them disappear - so you could actually just put the mouseout directly on the elements so when you leave any of them, then all links close, and then when you hover over the reast of the links, they also close the submenu..
+
+// alternatively you could nest the submenu inside a div and when you mouseout from 'that' div, then just hide the div, and then use mouseover on any other div apart from the services heading, for closing the same div
